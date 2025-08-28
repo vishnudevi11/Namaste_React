645651a2a64a8fb594b06a70f39a1eb799084fb0
vishnudevi11 <vishnudevis56@gmail.com>
2025-08-28 17:20:15 +0530

    episode-01

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-const heading=React.createElement("h1",{},"Hello World!");
+//we created only one element
+
+const heading=React.createElement
+("h1",{id:"heading"},"Hello World!");
+console.log(heading);
         const root=ReactDOM.createRoot(document.getElementById("root"));
         root.render(heading);
+
+{/* <div id="parent">
+    <div id="child">
+        <h1>i'm h1 tag</h1>
+    </div>
+</div> */}
+
+const container=React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"I'm an h1 tag" ),React.createElement("h2",{},"I'm an h2 tag" )])],
+    [React.createElement("div",{id:"child1"},
+        [React.createElement("h1",{},"I'm an h1 tag" ),React.createElement("h2",{},"I'm an h2 tag" )])]);
+
+const root1=ReactDOM.createRoot(document.getElementById("root"));
+root1.render(container);
